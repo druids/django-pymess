c156ae79ba2a7e5968c86235a192bd1fd892196e
pecko <pecko@pecko.sk>
2019-01-11 09:02:51 +0100

    Add is_final_state field to DialerMessage

--- pymess/backend/dialer/daktela.py
+++ pymess/backend/dialer/daktela.py
@@ -1,18 +1,9 @@
-<<<<<<< a7789e978d0a2ea6aa679a1ab5b66e73d62d3ecb
-from datetime import timedelta
-
 from django.utils import timezone as tz
 from django.utils.encoding import force_text
-from django.utils.timezone import now
-=======
-from django.utils import timezone as tz
-from django.utils.encoding import force_text
->>>>>>> Fixed dialer daktela backend, idle messages and state mapping
 
 from pymess.backend.dialer import DialerBackend
 from pymess.config import settings
 from pymess.models import DialerMessage
-from pymess.utils import fullname
 from pymess.utils.logged_requests import generate_session
 
 
@@ -60,6 +51,7 @@ class DaktelaDialerBackend(DialerBackend):
                 resp_message_state = str(DialerMessage.STATE.NOT_ASSIGNED)
             message_state = settings.DIALER_DAKTELA.STATES_MAPPING[resp_message_state]
             message_error = resp_json['error'] if len(resp_json['error']) else None
+            tts_processed = resp_json['result']['customFields']['ttsprocessed'][0]
 
             try:
                 self.update_message(
