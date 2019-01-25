783700a6721ab9c8c5f119a08d426946c2c87db2
pecko <pecko@pecko.sk>
2019-01-25 13:59:15 +0100

    Fix NotImplementedError

--- pymess/backend/dialer/dummy.py
+++ pymess/backend/dialer/dummy.py
@@ -7,5 +7,8 @@ class DummyDialerBackend(DialerBackend):
     Dummy dialer backend used for testing environments. Backend only logs messages to the database.
     """
 
+    def _update_dialer_states(self, messages):
+        pass
+
     def publish_message(self, message):
         self.update_message(message, state=DialerMessage.STATE.DEBUG)
--- pymess/backend/sms/dummy.py
+++ pymess/backend/sms/dummy.py
@@ -1,5 +1,3 @@
-from chamber.shortcuts import change_and_save
-
 from pymess.backend.sms import SMSBackend
 from pymess.models import OutputSMSMessage
 
@@ -8,6 +6,9 @@ class DummySMSBackend(SMSBackend):
     """
     Dummy SMS backend used for testing environments. Backend only logs messages to the database.
     """
+
+    def _update_sms_states(self, messages):
+        pass
 
     def publish_message(self, message):
         self.update_message(message, state=OutputSMSMessage.STATE.DEBUG)
