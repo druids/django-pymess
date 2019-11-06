8be2a2d9a42f1626425830df5d2890df049ffe94
matllubos <matllubos@gmail.com>
2019-11-06 17:08:22 +0100

    Used db locking instead of file lock

--- pymess/management/commands/send_emails_batch.py
+++ pymess/management/commands/send_emails_batch.py
@@ -2,13 +2,15 @@ import logging
 
 from datetime import timedelta
 
+from django.db import DatabaseError
 from django.db.models import Q
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.timezone import now
 
 from pymess.config import get_email_sender, settings
-from pymess.lockfile import FileLock, AlreadyLocked, LockTimeout
 from pymess.models import EmailMessage
+
+from chamber.utils.transaction import atomic_with_signals
 
 
 logger = logging.getLogger(__name__)
@@ -19,20 +21,10 @@ class Command(BaseCommand):
     Command for sending e-mails in batch.
     """
 
+    @atomic_with_signals
     def handle(self, *args, **options):
         if not settings.EMAIL_BATCH_SENDING:
             raise CommandError('Batch sending is turned off')
-
-        lock = FileLock(settings.EMAIL_BATCH_LOCK_FILE)
-        try:
-            self.stdout.write('acquiring lock...')
-            lock.acquire(settings.EMAIL_BATCH_LOCK_WAIT_TIMEOUT)
-        except AlreadyLocked:
-            logger.error('Send emails batch: lock already in place.')
-            raise CommandError('lock already in place. quitting.')
-        except LockTimeout:
-            logger.error('Send emails batch: waiting for the lock timed out.')
-            raise CommandError('waiting for the lock timed out. quitting.')
 
         email_sender = get_email_sender()
 
@@ -45,12 +37,15 @@ class Command(BaseCommand):
                     number_of_send_attempts__lte=settings.EMAIL_BATCH_MAX_NUMBER_OF_SEND_ATTEMPTS,
                     created_at__gte=now() - timedelta(seconds=settings.EMAIL_BATCH_MAX_SECONDS_TO_SEND)
                 )
-            ).order_by('created_at')[:settings.EMAIL_BATCH_SIZE]
+            ).select_for_update(nowait=True).order_by('created_at')[:settings.EMAIL_BATCH_SIZE]
             for message in messages_to_send:
-                email_sender.publish_message(message)
-                sent += 1
+                try:
+                    email_sender.publish_message(message)
+                    sent += 1
+                except Exception as ex:
+                    # Rollback shoud not be applied for already send e-mails
+                    logger.exception(ex)
             self.stdout.write('{} e-mails sent'.format(sent))
-        finally:
-            self.stdout.write('releasing lock...')
-            lock.release()
-            self.stdout.write('released.')
+        except DatabaseError:
+            self.stdout.write('e-mail messages are already locked')
+
