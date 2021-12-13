249a3b022ab1bac56178c262e0366a9880a3c75c
Luboš Mátl <matllubos@gmail.com>
2021-12-13 10:01:56 +0100

    Speed up sync email template body migration

--- pymess/utils/migrations.py
+++ pymess/utils/migrations.py
@@ -6,18 +6,25 @@ from chamber.shortcuts import change_and_save
 from pymess.config import settings
 
 
-class SyncEmailTemplate:
+def get_email_template_body_from_file(email_template_slug):
+    with codecs.open(os.path.join(
+            os.path.join(settings.EMAIL_HTML_DATA_DIRECTORY),
+            '{}.html'.format(email_template_slug)), 'r', encoding='utf-8-sig') as file:
+        return file.read()
 
-    def __init__(self, template_slug):
-        self.template_slug = template_slug
+
+class SyncEmailTemplates:
+
+    def __init__(self, template_slugs=None):
+        self.template_slugs = template_slugs
 
     def __call__(self, apps, schema_editor):
-        directory = os.path.join(settings.EMAIL_HTML_DATA_DIRECTORY)
-
-        with codecs.open(os.path.join(directory, '{}.html'.format(self.template_slug)), 'r',
-                         encoding='utf-8-sig') as file:
-            template_obj = apps.get_model(*settings.EMAIL_TEMPLATE_MODEL.split('.')).objects.get(
-                slug=self.template_slug
-            )
-            change_and_save(template_obj, body=file.read())
+        email_template_class = apps.get_model(*settings.EMAIL_TEMPLATE_MODEL.split('.'))
+        email_template_qs = email_template_class.objects.all()
+        if self.template_slugs:
+            email_template_qs = email_template_qs.filter(slug__in=self.template_slugs)
+        email_template_list = list(email_template_qs)
+        for email_template in email_template_list:
+            email_template.body = get_email_template_body_from_file(email_template.slug)
+        email_template_class.objects.bulk_update(email_template_list, ['body'])
 
